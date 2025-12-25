77a24b00be6e396ca81f15cbab5a9368a28d11de
Tanner Legasse <tanner.legasse@gmail.com>
2025-12-25 00:19:25 -0800

    Refactoring and making things a little simpler

--- public/script.js
+++ public/script.js
@@ -1,69 +1,72 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const iv = window.crypto.getRandomValues(new Uint8Array(12));
-    const key = document.getElementById("key")
-    const raw = document.getElementById("raw")
-    const decryptSubmit = document.getElementById("decrypt-submit")
-    const encrypted = document.getElementById("encrypted")
-    const salt = new Date().getTime()
-    const encoder = new TextEncoder();
-    const decoder = new TextDecoder("utf-8");
-    const randomPass = window.crypto.getRandomValues(new Uint8Array(12));
+class SafeSend {
+    constructor() {
+        this.encoder = new TextEncoder("utf-8");
+        this.decoder = new TextDecoder("utf-8");
 
-    const triggerEncryption = async () => {
-        const derivedKey = await deriveKey(key.value, salt)
-        const payload = raw.value
-        const encryptedPayload = await getEncryptedPayload(payload, derivedKey)
+        this.setupElements()
+        this.setupEncrpytionVals()
+        this.setupListeners()
+    }
+
+    setupElements() {
+        this.userInput = document.getElementById("raw")
+        this.decryptSubmit = document.getElementById("decrypt-submit")
+        this.encrypted = document.getElementById("encrypted")
+    }
+
+    async setupEncrpytionVals() {
+        this.iv = window.crypto.getRandomValues(new Uint8Array(12));
+        this.key = await window.crypto.subtle.generateKey(
+            { name: "AES-GCM", length: 256 },
+            true,
+            ["encrypt", "decrypt"]
+        );
+    }
+
+    async exportKey() {
+        const raw = await window.crypto.subtle.exportKey("raw", this.key);
+        return btoa(String.fromCharCode(...new Uint8Array(raw)));
+    }
+
+    async importKey(base64) {
+        const raw = Uint8Array.from(atob(base64), c => c.charCodeAt(0));
+        return await window.crypto.subtle.importKey(
+            "raw",
+            raw,
+            "AES-GCM",
+            false,
+            ["decrypt"]
+        );
+    }
+
+    async triggerEncryption() {
+        const payload = this.userInput.value
+        const encryptedPayload = await this.getEncryptedPayload(payload)
 
         encrypted.value = JSON.stringify({
-            keyStr: key.value,
-            salt: salt,
-            iv: btoa(String.fromCharCode(...iv)),
+            keyStr: await this.exportKey(),
+            iv: btoa(String.fromCharCode(...this.iv)),
             payload: encryptedPayload
         })
     }
 
-    const triggerDecryption = async () => {
+    async triggerDecryption() {
         const encryptedStr = encrypted.value
-        const { keyStr, salt, iv: ivBase64, payload } = JSON.parse(encryptedStr)
+        const { keyStr, iv: ivBase64, payload } = JSON.parse(encryptedStr)
         const iv = Uint8Array.from(atob(ivBase64), c => c.charCodeAt(0));
-        const derivedKey = await deriveKey(keyStr, salt)
+        this.key = await this.importKey(keyStr)
 
-        getDecryptedPayload(derivedKey, iv, payload)
+        console.log(await this.getDecryptedPayload(payload, iv))
     }
 
-    const deriveKey = async (password, salt) => {
-        const keyMaterial = await window.crypto.subtle.importKey(
-            "raw",
-            encoder.encode(password),
-            "PBKDF2",
-            false,
-            ["deriveKey"]
-        );
-
-        return await window.crypto.subtle.deriveKey(
-            {
-                name: "PBKDF2",
-                salt: encoder.encode(salt.toString()), // Store this with your data
-                iterations: 100000,
-                hash: "SHA-256"
-            },
-            keyMaterial,
-            { name: "AES-GCM", length: 256 },
-            false,
-            ["encrypt", "decrypt"]
-        );
-    };
-
-    const getEncryptedPayload = async (payload, derivedKey) => {
-        const encodedPayload = encoder.encode(payload);
-
+    async getEncryptedPayload(payload) {
         const encryptedPayload = await window.crypto.subtle.encrypt(
             {
                 name: "AES-GCM",
-                iv
+                iv: this.iv
             },
-            derivedKey,
-            encodedPayload
+            this.key,
+            this.encoder.encode(payload)
         );
 
         let buffer = new Uint8Array(encryptedPayload, 0, encryptedPayload.byteLength);
@@ -71,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
         return base64
     }
 
-    const getDecryptedPayload = async (derivedKey, iv, payload) => {
+    async getDecryptedPayload(payload, iv) {
         payload = Uint8Array.from(atob(payload), c => c.charCodeAt(0));
 
         let decrypted = await window.crypto.subtle.decrypt(
@@ -79,20 +82,20 @@ document.addEventListener("DOMContentLoaded", () => {
                 name: "AES-GCM",
                 iv
             },
-            derivedKey,
+            this.key,
             payload.buffer
         );
 
         const arr = new Uint8Array(decrypted)
-        return decoder.decode(arr)
+        return this.decoder.decode(arr)
     }
 
-    key.value = salt + randomPass
+    setupListeners() {
+        this.userInput.addEventListener("change", () => this.triggerEncryption())
+        this.encrypted.addEventListener("change", () => this.triggerDecryption())
+    }
+}
 
-    raw.addEventListener("change", () => triggerEncryption())
-    encrypted.addEventListener("change", () => triggerDecryption())
-
-    triggerEncryption()
-
-    decryptSubmit.addEventListener("click", () => triggerDecryption())
+document.addEventListener("DOMContentLoaded", () => {
+    new SafeSend()
 })
